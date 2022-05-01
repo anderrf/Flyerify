34147b46e61b39864a3c15f78a86b9f88116257a
anderrf <anderson_rocha_farias@outlook.com>
2022-05-01 15:41:14 -0300

    Creation of templates list page

--- Flyerify/Controllers/HomeController.cs
+++ Flyerify/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Templates()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
